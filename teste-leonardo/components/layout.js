aa9fcf1271eaf6fa4e654ff8b7b7848ed2408155
zerinoid <leo.zerino@gmail.com>
2020-12-21 19:17:07 -0300

    Row no layout para remover padding

--- teste-leonardo/components/layout.js
+++ teste-leonardo/components/layout.js
@@ -4,6 +4,7 @@ import Footer from "./footer"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
 
 export default function Layout({ children }) {
     return (
@@ -12,20 +13,22 @@ export default function Layout({ children }) {
                 {/* <link rel="icon" href="/icon.png" /> */}
                 <meta name="description" content="Teste petz" />
             </Head>
-            <Navbar>
-                <Navbar.Brand href="#">
-                    <img alt="Teste Inovação Logo" src="/images/LOGO.svg" />
-                </Navbar.Brand>
-                <Nav className="ml-auto">
-                    <Nav.Link href="#">O desafio</Nav.Link>
-                    <Nav.Link href="#">Recomendações</Nav.Link>
-                    <Nav.Link href="#">Feedback</Nav.Link>
-                    <Nav.Link href="#">A equipe</Nav.Link>
-                    <Nav.Link href="#">Comentários</Nav.Link>
-                </Nav>
-            </Navbar>
-            <main>{children}</main>
-            <Footer></Footer>
+            <Row>
+                <Navbar className={styles.navbar} sticky="top">
+                    <Navbar.Brand href="#">
+                        <img alt="Teste Inovação Logo" src="/images/LOGO.svg" />
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#">O desafio</Nav.Link>
+                        <Nav.Link href="#">Recomendações</Nav.Link>
+                        <Nav.Link href="#">Feedback</Nav.Link>
+                        <Nav.Link href="#">A equipe</Nav.Link>
+                        <Nav.Link href="#">Comentários</Nav.Link>
+                    </Nav>
+                </Navbar>
+                <main>{children}</main>
+                <Footer></Footer>
+            </Row>
         </Container>
     )
 }
