6e3fcbb79f88faa0c0952cdb9ce087e74766c631
zerinoid <leo.zerino@gmail.com>
2020-12-21 02:07:36 -0300

    layout importado na home; scss dedicado criado

--- teste-leonardo/components/layout.js
+++ teste-leonardo/components/layout.js
@@ -1,17 +1,20 @@
 import Head from "next/head"
 import Link from "next/link"
+import styles from "./layout.module.scss"
+import Footer from "./footer"
 
 export default function Layout({ children, home }) {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 {/* <link rel="icon" href="/icon.png" /> */}
                 <meta name="description" content="Teste petz" />
             </Head>
             <nav>
-                <img alt="" src="/LOGO.svg" />
+                <img alt="" src="/images/LOGO.svg" />
             </nav>
             <main>{children}</main>
+            <Footer></Footer>
         </div>
     )
 }
--- teste-leonardo/pages/index.js
+++ teste-leonardo/pages/index.js
@@ -1,18 +1,16 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.scss"
-import layout from "../components/layout"
+import Layout from "../components/layout"
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <Layout>
             <Head>
                 <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>conteudo principal</main>
-
-            <footer className={styles.footer}>footer</footer>
-        </div>
+            <p>conteudo principal</p>
+        </Layout>
     )
 }
