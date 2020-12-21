31b8d8e013eb5ef03f66da5f482e903f29130bd4
zerinoid <leo.zerino@gmail.com>
2020-12-21 02:51:53 -0300

    react-bootstrap instalado e implementado no layout

--- teste-leonardo/components/layout.js
+++ teste-leonardo/components/layout.js
@@ -2,19 +2,23 @@ import Head from "next/head"
 import Link from "next/link"
 import styles from "./layout.module.scss"
 import Footer from "./footer"
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
 
 export default function Layout({ children, home }) {
     return (
-        <div className={styles.container}>
+        <Container>
             <Head>
                 {/* <link rel="icon" href="/icon.png" /> */}
                 <meta name="description" content="Teste petz" />
             </Head>
-            <nav>
-                <img alt="" src="/images/LOGO.svg" />
-            </nav>
+            <Navbar>
+                <Navbar.Brand href="#">
+                    <img alt="Teste Inovação Logo" src="/images/LOGO.svg" />
+                </Navbar.Brand>
+            </Navbar>
             <main>{children}</main>
             <Footer></Footer>
-        </div>
+        </Container>
     )
 }
