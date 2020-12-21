3f4530df1eee9b6146c7a2cbe2cc91ca02c36d1e
zerinoid <leo.zerino@gmail.com>
2020-12-21 04:24:36 -0300

    nav.items dentro de nav

--- teste-leonardo/components/layout.js
+++ teste-leonardo/components/layout.js
@@ -19,11 +19,13 @@ export default function Layout({ children, home }) {
                 <Navbar.Brand href="#">
                     <img alt="Teste Inovação Logo" src="/images/LOGO.svg" />
                 </Navbar.Brand>
-                <Nav.Link href="#">O desafio</Nav.Link>
-                <Nav.Link href="#">Recomendações</Nav.Link>
-                <Nav.Link href="#">Feedback</Nav.Link>
-                <Nav.Link href="#">A equipe</Nav.Link>
-                <Nav.Link href="#">Comentários</Nav.Link>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#">O desafio</Nav.Link>
+                    <Nav.Link href="#">Recomendações</Nav.Link>
+                    <Nav.Link href="#">Feedback</Nav.Link>
+                    <Nav.Link href="#">A equipe</Nav.Link>
+                    <Nav.Link href="#">Comentários</Nav.Link>
+                </Nav>
             </Navbar>
             <main>{children}</main>
             <Footer></Footer>
