52589c2ad081914b601cfc535b783dd719111673
zerinoid <leo.zerino@gmail.com>
2020-12-22 20:08:32 -0300

    input bootstrap implementado

--- teste-leonardo/components/feedback.js
+++ teste-leonardo/components/feedback.js
@@ -1,11 +1,28 @@
 import styles from "./feedback.module.scss"
+import InputGroup from "react-bootstrap/InputGroup"
+import FormControl from "react-bootstrap/FormControl"
+import Button from "react-bootstrap/Button"
 
 export default function Feedback() {
     return (
         <div className="max-width">
             <div className={styles.container}>
                 <h3>Quer um feedback?</h3>
-                <p>Preencha seu email abaixo</p>
+                <p>Preencha seu email abaixo para receber</p>
+                <p>o feedback da sua avaliação.</p>
+                <p>Estamos tocendo por você!</p>
+                <InputGroup>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Digite seu email aqui..."
+                            aria-label="Digite seu email aqui..."
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-secondary">Enviar</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </InputGroup>
             </div>
         </div>
     )
