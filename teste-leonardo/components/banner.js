7289d71fb30a39249b376cdfba40755bc973adcc
zerinoid <leo.zerino@gmail.com>
2020-12-22 15:36:04 -0300

    max-width em banner

--- teste-leonardo/components/banner.js
+++ teste-leonardo/components/banner.js
@@ -1,5 +1,9 @@
 import styles from "./banner.module.scss"
 
 export default function Banner() {
-    return <div className={styles.banner}>texto etc</div>
+    return (
+        <div className={styles.banner}>
+            <div className="max-width">texto etc</div>
+        </div>
+    )
 }
