73f1cc3b83852608d50a2855ccc5be82a7647e6b
zerinoid <leo.zerino@gmail.com>
2020-12-23 18:21:12 -0300

    quebras de linha

--- teste-leonardo/components/equipe.js
+++ teste-leonardo/components/equipe.js
@@ -13,70 +13,70 @@ export default function Equipe() {
             id: 2,
             name: "Diogo Fraga",
             age: "30",
-            role: "Gestor de Inovação\n Líder ténico",
+            role: "Gestor de Inovação<br/>Líder ténico",
             padding: 245
         },
         {
             id: 3,
             name: "Bia Ferreira",
             age: "28",
-            role: "Designer de Interface",
+            role: "Designer de<br/> Interface",
             padding: 140
         },
         {
             id: 4,
             name: "Michael Bravo",
             age: "34",
-            role: "Desenvolvedor Backend",
+            role: "Desenvolvedor<br/>Backend",
             padding: 235
         },
         {
             id: 5,
             name: "Bianca Silva",
             age: "33",
-            role: "Analista de Qualidade",
+            role: "Analista de <br/>Qualidade",
             padding: 140
         },
         {
             id: 6,
             name: "Augusto Costa",
             age: "24",
-            role: "Desenvolvedor Frontend",
+            role: "Desenvolvedor <br/>Frontend",
             padding: 240
         },
         {
             id: 7,
             name: "Marco Dias",
             age: "41",
-            role: "Desenvolvedor Backend",
+            role: "Desenvolvedor <br/>Backend",
             padding: 240
         },
         {
             id: 8,
             name: "Miguel Silva",
             age: "32",
-            role: "Desenvolvedor Backend",
+            role: "Desenvolvedor <br/>Backend",
             padding: 220
         },
         {
             id: 9,
             name: "Carlos Gomes",
             age: "46",
-            role: "Desenvolvedor Fullstack",
+            role: "Desenvolvedor <br/>Fullstack",
             padding: 140
         },
         {
             id: 10,
             name: "Cristiano Souza",
             age: "21",
-            role: "Analista de Qualidade",
+            role: "Analista de <br/>Qualidade",
             padding: 140
         },
         {
             id: 11,
             name: "Bruno Lima",
             age: "36",
-            role: "Designer de Interface",
+            role: "Designer de <br/>Interface",
             padding: 30
         }
     ]
@@ -87,7 +87,7 @@ export default function Equipe() {
                 <h3>
                     {item.name}, {item.age}
                 </h3>
-                <p>{item.role}</p>
+                <p dangerouslySetInnerHTML={{ __html: item.role }} />
             </div>
         )
     })
