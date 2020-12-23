a1ac48b5221c75cae8c84d338e7ceb88fdfdd720
zerinoid <leo.zerino@gmail.com>
2020-12-23 20:19:46 -0300

    react-bs em contato

--- teste-leonardo/components/contato.js
+++ teste-leonardo/components/contato.js
@@ -1,16 +1,23 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 export default function Contato() {
     return (
-        <div>
-            <div>
-                <h3>Gostou do teste?</h3>
-                <p>
-                    Conte para nós oque você gostou no teste e o que você
-                    mudaria para melhor avaliar um cadidato à essa vaga. Sua
-                    opinião é importante para nós, queremos os melhores no nosso
-                    time.
-                </p>
-            </div>
+        <Container className="max-width">
+            <Row>
+                <Col>
+                    <h3>Gostou do teste?</h3>
+                    <p>
+                        Conte para nós oque você gostou no teste e o que você
+                        mudaria para melhor avaliar um cadidato à essa vaga. Sua
+                        opinião é importante para nós, queremos os melhores no
+                        nosso time.
+                    </p>
+                </Col>
+                <Col>Form</Col>
+            </Row>
             <div></div>
-        </div>
+        </Container>
     )
 }
