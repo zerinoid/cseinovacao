652232581ed9d95715b1757010aed124d5ce09b7
zerinoid <leo.zerino@gmail.com>
2020-12-23 17:57:49 -0300

    desenvolvimento component equipe

--- teste-leonardo/components/equipe.js
+++ teste-leonardo/components/equipe.js
@@ -2,61 +2,94 @@ import Masonry from "react-masonry-css"
 
 export default function Equipe() {
     let items = [
-        { id: 1, name: "João Pereira", age: "30", role: "Desenvolvedor" },
+        {
+            id: 1,
+            name: "João Pereira",
+            age: "30",
+            role: "Desenvolvedor",
+            padding: 140
+        },
         {
             id: 2,
-            name: "Bianca Silva",
-            age: "33",
-            role: "Analista de Qualidade"
+            name: "Diogo Fraga",
+            age: "30",
+            role: "Gestor de Inovação\n Líder ténico",
+            padding: 245
         },
         {
             id: 3,
-            name: "Carlos Gomes",
-            age: "46",
-            role: "Desenvolvedor Fullstack"
+            name: "Bia Ferreira",
+            age: "28",
+            role: "Designer de Interface",
+            padding: 140
         },
         {
             id: 4,
-            name: "Diogo Fraga",
-            age: "30",
-            role: "Gestor de Inovação\n Líder ténico"
+            name: "Michael Bravo",
+            age: "34",
+            role: "Desenvolvedor Backend",
+            padding: 235
         },
         {
             id: 5,
-            name: "Augusto Costa",
-            age: "24",
-            role: "Desenvolvedor Frontend"
+            name: "Bianca Silva",
+            age: "33",
+            role: "Analista de Qualidade",
+            padding: 140
         },
         {
             id: 6,
-            name: "Bia Ferreira",
-            age: "28",
-            role: "Designer de Interface"
+            name: "Augusto Costa",
+            age: "24",
+            role: "Desenvolvedor Frontend",
+            padding: 240
         },
-        { id: 7, name: "Marco Dias", age: "41", role: "Desenvolvedor Backend" },
+        {
+            id: 7,
+            name: "Marco Dias",
+            age: "41",
+            role: "Desenvolvedor Backend",
+            padding: 240
+        },
         {
             id: 8,
-            name: "Cristiano Souza",
-            age: "21",
-            role: "Analista de Qualidade"
+            name: "Miguel Silva",
+            age: "32",
+            role: "Desenvolvedor Backend",
+            padding: 220
         },
         {
             id: 9,
-            name: "Michael Bravo",
-            age: "34",
-            role: "Desenvolvedor Backend"
+            name: "Carlos Gomes",
+            age: "46",
+            role: "Desenvolvedor Fullstack",
+            padding: 140
         },
         {
             id: 10,
-            name: "Miguel Silva",
-            age: "32",
-            role: "Desenvolvedor Backend"
+            name: "Cristiano Souza",
+            age: "21",
+            role: "Analista de Qualidade",
+            padding: 140
         },
-        { id: 11, name: "Bruno Lima", age: "36", role: "Designer de Interface" }
+        {
+            id: 11,
+            name: "Bruno Lima",
+            age: "36",
+            role: "Designer de Interface",
+            padding: 30
+        }
     ]
 
     items = items.map(function (item) {
-        return <div key={item.id}>{item.name}</div>
+        return (
+            <div key={item.id} style={{ paddingTop: item.padding }}>
+                <h3>
+                    {item.name}, {item.age}
+                </h3>
+                <p>{item.role}</p>
+            </div>
+        )
     })
 
     return (
