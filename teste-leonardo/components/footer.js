9c3c3dab044bb803af90b5ef9c8d6f4ec25e431b
zerinoid <leo.zerino@gmail.com>
2020-12-23 22:10:08 -0300

    BS no footer

--- teste-leonardo/components/footer.js
+++ teste-leonardo/components/footer.js
@@ -1,5 +1,17 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import styles from "./footer.module.scss"
 
-export default function footer() {
-    return <footer className={styles.footer}>footer</footer>
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <Container className="max-width">
+                <Row>
+                    <Col md={9}>texto</Col>
+                    <Col md={3}>redes</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
