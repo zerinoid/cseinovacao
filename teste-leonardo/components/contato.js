6dd898e7764cd836fee0fb7ffff6002f6ed7fc09
zerinoid <leo.zerino@gmail.com>
2020-12-23 21:20:41 -0300

    botão e margens

--- teste-leonardo/components/contato.js
+++ teste-leonardo/components/contato.js
@@ -2,13 +2,14 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
 export default function Contato() {
     return (
         <Container className="max-width">
             <Row>
                 <Col>
-                    <h3>Gostou do teste?</h3>
+                    <h3 className="mb-4">Gostou do teste?</h3>
                     <p>
                         Conte para nós oque você gostou no teste e o que você
                         mudaria para melhor avaliar um cadidato à essa vaga. Sua
@@ -26,7 +27,7 @@ export default function Contato() {
                                 <Form.Control placeholder="Sobrenome" />
                             </Col>
                         </Form.Row>
-                        <Form.Row>
+                        <Form.Row className="mb-3">
                             <Col>
                                 <Form.Control
                                     as="textarea"
@@ -35,6 +36,7 @@ export default function Contato() {
                                 />
                             </Col>
                         </Form.Row>
+                        <Button className="ml-auto d-block px-5">ENVIAR</Button>
                     </Form>
                 </Col>
             </Row>
