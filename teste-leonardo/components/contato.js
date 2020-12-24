2e0c323af145ac68d17ebb0dd809b1393a920cea
zerinoid <leo.zerino@gmail.com>
2020-12-23 21:46:26 -0300

    margin e padding top de contato

--- teste-leonardo/components/contato.js
+++ teste-leonardo/components/contato.js
@@ -3,10 +3,11 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import styles from "./contato.module.scss"
 
 export default function Contato() {
     return (
-        <Container className="max-width">
+        <Container className={`max-width ${styles.container}`}>
             <Row>
                 <Col>
                     <h3 className="mb-4">Gostou do teste?</h3>
