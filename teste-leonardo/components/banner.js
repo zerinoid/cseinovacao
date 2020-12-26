b2f9d12e8b6074d0a45eb963b8482e7371c05f07
zerinoid <leo.zerino@gmail.com>
2020-12-26 19:08:00 -0300

    texto banner

--- teste-leonardo/components/banner.js
+++ teste-leonardo/components/banner.js
@@ -3,7 +3,20 @@ import styles from "./banner.module.scss"
 export default function Banner() {
     return (
         <div className={styles.banner}>
-            <div className="max-width">texto etc</div>
+            <div className="max-width">
+                <p>
+                    Criamos produtos <span>incríveis</span>
+                </p>
+                <p>
+                    que vão <strong>impactar</strong> o mercado.
+                </p>
+                <p>
+                    <strong>Você está pronto</strong> para
+                </p>
+                <p>
+                    <strong>fazer parte desse time?</strong>
+                </p>
+            </div>
         </div>
     )
 }
