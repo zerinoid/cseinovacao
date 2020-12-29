96041a3c8954d563b492abb96f1a31ec5d13428c
zerinoid <leo.zerino@gmail.com>
2020-12-29 03:02:35 -0300

    BP no slick

--- teste-leonardo/components/carousel.js
+++ teste-leonardo/components/carousel.js
@@ -7,15 +7,26 @@ import Item from "./carouselItem"
 export default function Carousel() {
     var slickOptions = {
         centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 2,
+        slidesToScroll: 3,
         infinite: false,
-        dots: false
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    fade: true
+                }
+            }
+        ]
     }
+
     return (
         <div className={styles.carousel}>
             <div className="max-width">
-                <h3>Recomendações:</h3>
+                <h4>Recomendações:</h4>
                 <Slider {...slickOptions}>
                     <Item n={1} title={"Organize seu tempo"}>
                         Verifique quanto tempo você tem disponível para realizar
