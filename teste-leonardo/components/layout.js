5c1eac552efa11218224f5aa9af8c622f4403b64
zerinoid <leo.zerino@gmail.com>
2020-12-28 21:26:54 -0300

    removido div max width em layout

--- teste-leonardo/components/layout.js
+++ teste-leonardo/components/layout.js
@@ -19,21 +19,16 @@ export default function Layout({ children }) {
             </Head>
             <Row className={styles.columnDir}>
                 <Navbar className={styles.navbar} sticky="top">
-                    <div className="max-width nav-flex">
-                        <Navbar.Brand href="#">
-                            <img
-                                alt="Teste Inovação Logo"
-                                src="/images/LOGO.svg"
-                            />
-                        </Navbar.Brand>
-                        <Nav className="ml-auto">
-                            <Nav.Link href="#">O desafio</Nav.Link>
-                            <Nav.Link href="#">Recomendações</Nav.Link>
-                            <Nav.Link href="#">Feedback</Nav.Link>
-                            <Nav.Link href="#">A equipe</Nav.Link>
-                            <Nav.Link href="#">Comentários</Nav.Link>
-                        </Nav>
-                    </div>
+                    <Navbar.Brand href="#">
+                        <img alt="Teste Inovação Logo" src="/images/LOGO.svg" />
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#">O desafio</Nav.Link>
+                        <Nav.Link href="#">Recomendações</Nav.Link>
+                        <Nav.Link href="#">Feedback</Nav.Link>
+                        <Nav.Link href="#">A equipe</Nav.Link>
+                        <Nav.Link href="#">Comentários</Nav.Link>
+                    </Nav>
                 </Navbar>
                 <main>{children}</main>
                 <Footer></Footer>
