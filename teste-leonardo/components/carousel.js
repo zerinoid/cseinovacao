4be9490a1e944a280e59aa4a392df7273120d71c
zerinoid <leo.zerino@gmail.com>
2020-12-29 04:44:15 -0300

    carousel com map

--- teste-leonardo/components/carousel.js
+++ teste-leonardo/components/carousel.js
@@ -6,6 +6,26 @@ import Item from "./carouselItem"
 import { useMediaQuery } from "react-responsive"
 
 export default function Carousel() {
+    let items = [
+        {
+            id: 1,
+            title: "Organize seu tempo",
+            content:
+                "Verifique quanto tempo você tem disponível para realizar o teste com eficiência então crie pequenas metas para manter o ritmo."
+        },
+        {
+            id: 2,
+            title: "Tire suas dúvidas",
+            content:
+                "Se houver alguma dúvida, procure o aplicador do teste, assim você terá melhores chances de entregar o que foi proposto."
+        },
+        {
+            id: 3,
+            title: "Seja Criativo",
+            content:
+                " Existem várias formas de resolver uma questão de html e css ou javascript, mostre que você domina a melhor. "
+        }
+    ]
     const isDesktop = useMediaQuery({ query: "(min-width: 576px)" })
 
     const NextArrow = props => {
@@ -67,20 +87,13 @@ export default function Carousel() {
             <div className="max-width">
                 <h4 className="mb-3">Recomendações:</h4>
                 <Slider {...slickOptions}>
-                    <Item n={1} title={"Organize seu tempo"}>
-                        Verifique quanto tempo você tem disponível para realizar
-                        o teste com eficiência então crie pequenas metas para
-                        manter o ritmo.
-                    </Item>
-                    <Item n={2} title={"Tire suas dúvidas"}>
-                        Se houver alguma dúvida, procure o aplicador do teste,
-                        assim você terá melhores chances de entregar o que foi
-                        proposto.
-                    </Item>
-                    <Item n={3} title={"Seja Criativo"}>
-                        Existem várias formas de resolver uma questão de html e
-                        css ou javascript, mostre que você domina a melhor.
-                    </Item>
+                    {items.map(item => {
+                        return (
+                            <Item key={item.id} n={item.id} title={item.title}>
+                                {item.content}
+                            </Item>
+                        )
+                    })}
                 </Slider>
             </div>
         </div>
